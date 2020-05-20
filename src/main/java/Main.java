48dbdd478b5195473414ded50bef426c2336c7be
serhii-slobodianiuk <sergey.slobodianiuk@gmail.com>
2020-05-20 19:37:05 +0300

    Refactoring is done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,13 @@
 import java.io.File;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 
 public class Main {
 
-//    private static final ExecutorService executor = Executors.newCachedThreadPool();
+    private static final ExecutorService executor = Executors.newCachedThreadPool();
 
     public static void main(String[] args) {
 
@@ -29,22 +27,23 @@ public class Main {
         destFile.getParentFile().mkdirs();
 
         List<String> userSourcePaths = SourceReader.getPathsFromSourceFile(sourceFileName);
-        Map<String, Long> pathsAndFilesCount = new ConcurrentHashMap<>(userSourcePaths.size());
 
-        ExecutorService executor = Executors.newCachedThreadPool();
+        InterruptThread interruptThread = new InterruptThread(executor);
+        executor.submit(interruptThread);
 
-        MultiThreadCountable countable =
-                new ParallelFileCounterService(executor, userSourcePaths, pathsAndFilesCount);
+        ParallelFileCounterService countable = new ParallelFileCounterService(executor, userSourcePaths);
         countable.createMultiThreadCount();
 
-        FormatConverter.createCSV(destFileName, pathsAndFilesCount);
+        Map<String, Long> pathsAndFilesCount = countable.getPathsAndFilesCount();
         printDirectory(pathsAndFilesCount);
+        FormatConverter.createCSV(destFileName, pathsAndFilesCount);
         executor.shutdown();
+
     }
 
     private static void printDirectory(Map<String, Long> pathsAndFilesCount) {
         int rowIndex = 0;
-
+//        System.out.println();
         System.out.printf("%-10s| %-11s| %s\n", "Row Index", "File count", "Paths");
         System.out.println("-------------------------------");
         for (String path : pathsAndFilesCount.keySet()) {
