0f29ea74d400708dd9e696b0e49d9223f3f452da
serhii-slobodianiuk <sergey.slobodianiuk@gmail.com>
2020-05-25 23:50:11 +0300

    Claas Printer is added

---
+++ src/main/java/Util/Printer.java
@@ -0,0 +1,17 @@
+package Util;
+
+import java.util.Map;
+
+public class Printer {
+    public static void printDirectory(Map<String, Long> pathsAndFilesCount) {
+        int rowIndex = 0;
+//        System.out.println();
+        System.out.printf("%-10s| %-11s| %s\n", "Row Index", "File count", "Paths");
+        System.out.println("-------------------------------");
+        for (String path : pathsAndFilesCount.keySet()) {
+            Long fileCount = pathsAndFilesCount.get(path);
+            rowIndex++;
+            System.out.printf("%-10d| %-11d| %s\n", rowIndex, fileCount, path);
+        }
+    }
+}
