69012b2a2c1e60f52bb2808bf9b02ecadde253c8
serhii-slobodianiuk <sergey.slobodianiuk@gmail.com>
2020-05-27 02:11:22 +0300

    GlobalKeyListener class is added

---
+++ src/main/java/KeyListenerHandle/GlobalKeyListener.java
@@ -0,0 +1,40 @@
+package KeyListenerHandle;
+
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+
+import java.util.concurrent.ExecutorService;
+
+public class GlobalKeyListener implements NativeKeyListener {
+
+    private final ExecutorService executor;
+
+    GlobalKeyListener(ExecutorService executor) {
+        this.executor = executor;
+    }
+
+    @Override
+    public void nativeKeyTyped(NativeKeyEvent event) {
+        // nothing to do
+    }
+
+    @Override
+    public void nativeKeyPressed(NativeKeyEvent event) {
+
+        if (event.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
+            try {
+                GlobalScreen.unregisterNativeHook();
+                executor.shutdownNow();
+            } catch (NativeHookException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void nativeKeyReleased(NativeKeyEvent event) {
+        // nothing to do
+    }
+}
