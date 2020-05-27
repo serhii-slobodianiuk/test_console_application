3ecede13c258fdcfd2b543742070d8cda4243aee
serhii-slobodianiuk <sergey.slobodianiuk@gmail.com>
2020-05-28 01:36:06 +0300

    renamed from SourceValidator ensureParentDirExists method is added all methods is refactored

---
+++ src/main/java/SourceData/Arguments.java
@@ -0,0 +1,49 @@
+package SourceData;/*
+ * A directory to validate an input data from specified by the user.
+ * */
+
+import java.io.File;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+public class Arguments {
+
+    private  String[] args;
+
+    public Arguments(String[] args) {
+        this.args = args;
+    }
+
+    public void checkNumberOfArgument() {
+        checkArgument(args.length == 2,
+                "Wrong number of arguments",
+                new IllegalArgumentException());
+    }
+
+    public File validateFile(String path) {
+        File file = new File(path);
+
+        if (!file.exists()) {
+            System.err.println(file + "...... file doesn't exist");
+            throw new IllegalArgumentException();
+
+        } else if (!file.canRead()) {
+            System.err.println(file + "...... file is unreadable");
+            throw new IllegalArgumentException();
+        }
+        return file;
+    }
+
+    public File sourceFile() {
+        return validateFile(args[0]);
+    }
+
+    public File destinationFile() {
+        return validateFile(args[1]);
+    }
+
+    public void ensureParentDirExists(File destinationFile){
+        destinationFile.getParentFile().mkdirs();
+    }
+
+}
