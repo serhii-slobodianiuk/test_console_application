7c33c62089b55a3b9d1e0d752b47a9e8d6c80904
serhii-slobodianiuk <sergey.slobodianiuk@gmail.com>
2020-06-02 21:12:12 +0300

    Refactoring is done

--- src/main/java/Util/ConsoleLogger.java
+++ src/main/java/Util/ConsoleLogger.java
@@ -8,7 +8,7 @@ import static java.util.logging.Level.OFF;
 
 public final class ConsoleLogger {
 
-    public static void disableLog(){
+    public static void disableLog() {
         Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
         logger.setLevel(OFF);
         logger.setUseParentHandlers(false);
